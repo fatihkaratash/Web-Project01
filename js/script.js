eee8b535522003ea456c90810d9d6b053c02a905
fatihkaratash <f.karatash.dev@gmail.com>
2024-05-17 12:14:35 +0300

    d11-v2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector("#menu-btn")
 
 
-window.onload = function(){
+//api - 1 (asagıya alınca bazıları calısıyor bazıları calısymıyor)
 
 menuBtn.addEventListener("click" , function(){
     navbar.classList.toggle("active");
@@ -17,6 +17,7 @@ menuBtn.addEventListener("click" , function(){
 
     });
 });
+
 
 
 document.getElementById("home-btn1").addEventListener("click", function() {
@@ -64,58 +65,9 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 });
 //js kontrolleri bitti
 
- //api//
-
- document.getElementById('getPriceBtn').addEventListener('click', function() {
-    fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd')
-        .then(response => response.json())
-        .then(data => {
-            const bitcoinPrice = data.bitcoin.usd;
-            const ethereumPrice = data.ethereum.usd;
-            const solanaPrice = data.solana.usd;
-
-            updatePrice('bitcoinPrice', `Bitcoin Fiyatı: $${bitcoinPrice}`);
-            updatePrice('ethereumPrice', `Ethereum Fiyatı: $${ethereumPrice}`);
-            updatePrice('solanaPrice', `Solana Fiyatı: $${solanaPrice}`);
-        })
-        .catch(error => {
-            console.error('Hata:', error);
-            updatePrice('bitcoinPrice', 'Bitcoin fiyatı alınamadı.');
-            updatePrice('ethereumPrice', 'Ethereum fiyatı alınamadı.');
-            updatePrice('solanaPrice', 'Solana fiyatı alınamadı.');
-        });
-});
-
-function updatePrice(elementId, text) {
-    const element = document.getElementById(elementId);
-    element.innerText = text;
-    element.classList.add('animate');
-    setTimeout(() => {
-        element.classList.remove('animate');
-    }, 1000);
-}
-//api//
 
 
-const apiKey = 'GAXUL624U6PUDBG4'; // Alpha Vantage API anahtarım
-document.getElementById('getPriceButton').addEventListener('click', function() {
-    getStockPrice('AAPL', 'applePrice');
-    getStockPrice('MSFT', 'microsoftPrice');
-    getStockPrice('GOOGL', 'googlePrice');
-});
 
-function getStockPrice(symbol, elementId) {
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            const price = data['Global Quote']['05. price'];
-            updatePrice(elementId, `${symbol} Fiyatı: $${price}`);
-        })
-        .catch(error => {
-            console.error('Hata:', error);
-            updatePrice(elementId, `${symbol} fiyatı alınamadı.`);
-        });
-}
+   
+ 
 
-//api bitti
-};
