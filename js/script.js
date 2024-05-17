057a60cc840ecf2753376beb498c240fc0273d00
fatihkaratash <f.karatash.dev@gmail.com>
2024-05-17 11:53:55 +0300

    d11 : Last updates

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 
 const navbar = document.querySelector(".navbar");
-
-// buttons //
-
-
 const menuBtn = document.querySelector("#menu-btn")
 
 
+window.onload = function(){
 
 menuBtn.addEventListener("click" , function(){
     navbar.classList.toggle("active");
@@ -36,6 +33,7 @@ $(document).ready(function(){
     $('.carousel').carousel();
   });
 
+//js kontrolleri
   function validateform() {
     var name = document.myform.name.value;
     var password = document.myform.password.value;
@@ -64,4 +62,60 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         document.getElementById("error-message").innerText = "Hatalı kullanıcı adı veya şifre!";
     }
 });
+//js kontrolleri bitti
 
+ //api//
+
+ document.getElementById('getPriceBtn').addEventListener('click', function() {
+    fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd')
+        .then(response => response.json())
+        .then(data => {
+            const bitcoinPrice = data.bitcoin.usd;
+            const ethereumPrice = data.ethereum.usd;
+            const solanaPrice = data.solana.usd;
+
+            updatePrice('bitcoinPrice', `Bitcoin Fiyatı: $${bitcoinPrice}`);
+            updatePrice('ethereumPrice', `Ethereum Fiyatı: $${ethereumPrice}`);
+            updatePrice('solanaPrice', `Solana Fiyatı: $${solanaPrice}`);
+        })
+        .catch(error => {
+            console.error('Hata:', error);
+            updatePrice('bitcoinPrice', 'Bitcoin fiyatı alınamadı.');
+            updatePrice('ethereumPrice', 'Ethereum fiyatı alınamadı.');
+            updatePrice('solanaPrice', 'Solana fiyatı alınamadı.');
+        });
+});
+
+function updatePrice(elementId, text) {
+    const element = document.getElementById(elementId);
+    element.innerText = text;
+    element.classList.add('animate');
+    setTimeout(() => {
+        element.classList.remove('animate');
+    }, 1000);
+}
+//api//
+
+
+const apiKey = 'GAXUL624U6PUDBG4'; // Alpha Vantage API anahtarım
+document.getElementById('getPriceButton').addEventListener('click', function() {
+    getStockPrice('AAPL', 'applePrice');
+    getStockPrice('MSFT', 'microsoftPrice');
+    getStockPrice('GOOGL', 'googlePrice');
+});
+
+function getStockPrice(symbol, elementId) {
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            const price = data['Global Quote']['05. price'];
+            updatePrice(elementId, `${symbol} Fiyatı: $${price}`);
+        })
+        .catch(error => {
+            console.error('Hata:', error);
+            updatePrice(elementId, `${symbol} fiyatı alınamadı.`);
+        });
+}
+
+//api bitti
+};
