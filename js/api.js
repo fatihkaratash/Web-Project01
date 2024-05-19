849affc427079977a7441df2b4e5e855c344c17d
fatihkaratash <f.karatash.dev@gmail.com>
2024-05-19 09:50:25 +0300

    ll

--- js/api.js
+++ js/api.js
@@ -53,6 +53,27 @@ function updatePrice(elementId, text) {
        });
        
        
-       
-       
-       //api//
+       //api-3//
+
+       async function fetchData(){
+
+        try{
+    
+            const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    
+            if(!response.ok){
+                throw new Error("Could not fetch resource");
+            }
+    
+            const data = await response.json();
+            const pokemonResmi = data.sprites.front_default;
+            const imgElement = document.getElementById("pokemonResmi");
+    
+            imgElement.src = pokemonResmi;
+            imgElement.style.display = "block";
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
